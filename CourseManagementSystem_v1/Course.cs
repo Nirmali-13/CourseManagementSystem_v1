4f3831a9be3de7c37bbd0c7d03c581de80e77b0d
Nirmali G. <UT01470@unicomtic.com>
2024-10-11 14:14:18 +0530

    first commit

---
+++ CourseManagementSystem_v1/Course.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseManagementSystem_v1
+{
+    internal class Course
+    {
+        private string courseId;
+        private string title;
+        private string Duration;
+        private decimal Price;
+        
+    }
+}
